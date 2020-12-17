005a949b5a9a90815ea5e01ea99a3aeae28f3d22
Nitja <ksenia.ossipova@gmail.com>
2020-12-17 13:14:57 +0200

    checkoboxid, unique sku

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -9,6 +9,8 @@ import { DatabaseService } from "../database/database.service";
 })
 export class AddPage {
 
+  notUnique = false;
+
   constructor(private database: DatabaseService) {}
 
   onSaveClick(form: NgForm) {
@@ -20,5 +22,15 @@ export class AddPage {
     this.database.synchronizeProducts(products);
 
     form.reset();
+  }
+
+  ckechUniquness(sku) {
+    this.notUnique = false;
+    let products = JSON.parse(localStorage.getItem("products")) || [];
+    products.forEach(product => {
+      if(product.sku == sku) {
+        this.notUnique = true;
+      }
+    });
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,27 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import { DatabaseService } from "../database/database.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-
   products;
-  
+
   constructor() {}
 
   ionViewDidEnter() {
-    // this.products = [
-    //     { sku: "JVC200123", name: 'Acme DISC', price: '100', type: "DVD-disk", attribute: "700", unit: "MB" },
-    //     { sku: "JVC200123", name: 'Acme DISC', price: '100', type: "DVD-disk", attribute: "700", unit: "MB"  },
-    //     { sku: "JVC200123", name: 'Acme DISC', price: '100', type: "DVD-disk", attribute: "700", unit: "MB"  }
-    // ];
-    //tut schitat iz bazq || [] 
-    //console.log (this.products);
-
     this.products = JSON.parse(localStorage.getItem("products")) || [];
-}
-
+  }
+  onMassDelete() {
+    for (let i = 0; i < this.products.length; i++) {
+      if (this.products[i].isChecked) {
+        this.products.splice(i, 1);
+        i--;
+      }
+    }
+  }
 }
